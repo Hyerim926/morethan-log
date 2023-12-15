acb952e94149d4efc80ad12d7d36c0cb977feafb
Hyerim926 <ekfrhsk96@icloud.com>
2023-12-15 19:16:17 +0900

    feat: 게시글 프로그레스바 추가

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -4,12 +4,15 @@ import styled from "@emotion/styled"
 
 const Logo = () => {
   return (
-    <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
-      {CONFIG.blog.title}
-    </StyledWrapper>
+      <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
+        {CONFIG.blog.title}
+      </StyledWrapper>
   )
 }
 
 export default Logo
 
-const StyledWrapper = styled(Link)``
+const StyledWrapper = styled(Link)`
+  font-size: 1.25rem;
+  font-weight: bold;
+`
